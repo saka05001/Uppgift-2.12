7a499d46ab3d6d33c5ef9301ec9a8f71d0b5771f
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:22:20 +0200

    Add project files.

---
+++ Uppgift 2.12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+ namespace Uppgift_2._12
+ {
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int avgift = 300;
+            Console.WriteLine("Hur många dagar ska du hyra den här bilen?");
+            int dagar = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            int km = int.Parse(Console.ReadLine());
+
+
+           
+
+            Console.WriteLine( avgift + dagar * 500 - 500 + km );
+
+        }
+    }
+ }
